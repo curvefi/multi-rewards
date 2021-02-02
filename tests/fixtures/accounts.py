e038208f0ea4d65b7b7a88d13e9a5b8c767714e2
Gerrit <gerrit@rezscore.com>
2021-02-01 18:43:29 -0800

    MultiRewards Tests v1

---
+++ tests/fixtures/accounts.py
@@ -0,0 +1,16 @@
+import pytest
+
+# Hi Alice
+@pytest.fixture(scope="session")
+def alice(accounts):
+    yield accounts[0]
+
+# Hey-a Bob
+@pytest.fixture(scope="session")
+def bob(accounts):
+    yield accounts[1]
+
+# Don't forget Charlie
+@pytest.fixture(scope="session")
+def charlie(accounts):
+    yield accounts[2]
